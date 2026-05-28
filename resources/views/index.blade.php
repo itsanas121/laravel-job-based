6fbb7ac14974d06aa7e6c7c1af15045c3ffb4070
Anas <anascs2023@gmail.com>
2026-05-28 04:15:41 +0300

    Blade Templating Engine

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <h2>Welcome to my page</h2>
+    <ul>
+        <li>
+            Today's Jobs
+        </li>
+        <li>
+            Treding Jobs
+        </li>
+    </ul>
+</x-layout>
